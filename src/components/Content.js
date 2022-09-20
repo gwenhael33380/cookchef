fedffca191fff2adb90f9c2bcdc7ab39d36bd799
gwenhael33380 <le.corre.gwen.hael@gmail.com>
2022-09-20 20:08:02 +0200

    fin de la mise en place des cards

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,7 +4,7 @@ function Content() {
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-      <div className="card p-20">
+      <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
           <div className={styles.elem}>Element</div>
           <div className={styles.elem}>Element</div>
