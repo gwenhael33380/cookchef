6dc576c021361dddce3bdaffff36d84a4fae8607
gwenhael33380 <le.corre.gwen.hael@gmail.com>
2022-09-20 19:23:59 +0200

    création du grid pour les cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-// import Content from "./components/Content";
+import Content from "./components/Content";
 import Footer from "./components/Footer";
 import styles from "./App.module.scss"
 function App() {
   return (
  <div className={`d-flex flex-column ${styles.appContainer}`}>
 <Header />
-{/* <Content /> */}
+<Content />
 <Footer /> 
  </div>
   );
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,40 @@
+import styles from "./Content.module.scss";
+
 function Content() {
-    return <div className="b3 flex-fill">Content</div>
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div className="card p-20">
+        <div className={styles.grid}>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
