3b3c0d866e25716552fd97a1462c933342895bee
gwenhael33380 <le.corre.gwen.hael@gmail.com>
2022-09-24 05:42:08 +0200

    mise en palce du components Articles

---
+++ src/components/Articles.js
@@ -0,0 +1,16 @@
+function Articles() {
+  return (
+    <div style={{ width: "700px" }}>
+      <h1 className="mb-20">Liste des articles</h1>
+      <div className="card p-20">
+        <h2 className="mb-10">Titre de l'article</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          blanditiis quibusdam iste, voluptate, facilis beatae cupiditate velit
+          quisquam unde maiores mollitia itaque eligendi omnis laudantium
+          numquam adipisci temporibus obcaecati quia.
+        </p>
+      </div>
+    </div>
+  );
+}
